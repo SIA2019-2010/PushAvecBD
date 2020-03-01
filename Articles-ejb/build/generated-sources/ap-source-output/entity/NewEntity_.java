6b79925c23497be43a59987a4e1c51af5ac20da4
agentTingfeng <58440222+agentTingfeng@users.noreply.github.com>
2020-03-01 20:34:19 +0100

    New

--- Articles-ejb/build/generated-sources/ap-source-output/entity/NewEntity_.java
+++ Articles-ejb/build/generated-sources/ap-source-output/entity/NewEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2020-03-01T20:04:01")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2020-03-01T20:30:10")
 @StaticMetamodel(NewEntity.class)
 public class NewEntity_ { 
 
